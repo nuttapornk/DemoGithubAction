9ed7f71f6dff0bc36b6401beab842daa91128f56
Nuttaporn kunsin <to_andata@hotmail.com>
2023-01-01 20:56:02 +0700

    update

---
+++ src/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+foreach (var item in RandomNumbers(15))
+    Console.WriteLine($"{item} {(item % 2 == 0 ? "EVEN" : "ODD")}");
+
+
+static int[] RandomNumbers(int times)
+{
+    Random random = new();   
+    List<int> result = new();
+    for (int i = 0; i < times; i++)
+        result.Add(random.Next(1, 10));
+    
+    return result.ToArray();
+}
